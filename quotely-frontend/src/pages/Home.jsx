f614ec91c2ef6e6ad7098ce6d82f40a6e72087b8
nana-boateng <44186742+nana-boateng@users.noreply.github.com>
2021-03-06 15:34:43 -0500

    Update homepage

--- quotely-frontend/src/pages/Home.jsx
+++ quotely-frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+    <div className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 h-screen">
       <h1>Quotely</h1>
     </div>
   );
